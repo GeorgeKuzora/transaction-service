212fbfa222cd89d4f6e5ce515f816313600ac6ae
GeorgiyKuzora <rafale87@gmail.com>
2024-06-30 17:25:42 +0300

    Добавляет метод create_transaction

--- src/app/in_memory_repository.py
+++ src/app/in_memory_repository.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from app.service import Transaction, TransactionReport
 
@@ -25,3 +26,31 @@ class InMemoryRepository:
         self.transactions_count: int = 0
         self.reports: list[TransactionReport] = []
         self.reports_count: int = 0
+
+    def create_transaction(self, transaction: Transaction) -> Transaction:
+        """
+        Создает запись о транзакции и возвращает ее.
+
+        Создает индексированную запись о транзакции и записывает ее в
+        список транзакций. Возвращает созданную индексированную запись
+        о транзакции.
+
+        Args:
+            transaction: Неиндексированный объект Transaction.
+
+        Returns:
+            Transaction: Индексированная запись о транзакции.
+        """
+        indexed_transaction = Transaction(
+            user_id=transaction.user_id,
+            amount=transaction.amount,
+            transacton_type=transaction.transacton_type,
+            timestamp=transaction.timestamp,
+            transaction_id=self.transactions_count,
+        )
+
+        self.transactions_count += 1
+        self.transactions.append(indexed_transaction)
+        logger.info(f'Добавил транзакцию {transaction} в хранилище')
+
+        return indexed_transaction
