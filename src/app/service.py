69aae2f34b0bada7f53c81544fb842a92e6761d0
GeorgiyKuzora <rafale87@gmail.com>
2024-06-30 17:09:47 +0300

    Добавляет Enum типов транзакций

--- src/app/service.py
+++ src/app/service.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 logger = logging.getLogger(__name__)
 
 
@@ -9,3 +10,14 @@ class RepositoryError(Exception):
     Импортировать в имплементации репозитория данных,
     для вызова исключения при ошибке доступа к данным.
     """
+
+
+class TransactionType(Enum):
+    """
+    Тип транзакции.
+
+    Может быть либо Продажа, либо Покупка.
+    """
+
+    SELL = 0
+    BUY = 1
