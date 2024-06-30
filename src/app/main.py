1256992ca07cb45802179eca4eabb6beb6ce2208
GeorgiyKuzora <rafale87@gmail.com>
2024-06-30 17:26:43 +0300

    Создает функцию main как точку входа в программу

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,46 @@
 import logging
+from datetime import datetime, timedelta
+
+from app.in_memory_repository import InMemoryRepository
+from app.service import TransactionService, TransactionType
 
 
-def get_greetings() -> str:
-    """Возвращает строку приветствия."""
-    return 'Hello World!'
+def main() -> None:
+    """Точка входа в программу."""
+    logger = logging.getLogger(__name__)
+    repository = InMemoryRepository()
+    service = TransactionService(repository)
+
+    transaction = service.create_transaction(
+        user_id=1,
+        amount=10,
+        transaction_type=TransactionType.SELL,
+    )
+    logger.info(f'Создана транзакция {transaction}')
+
+    transaction = service.create_transaction(
+        user_id=2,
+        amount=10,
+        transaction_type=TransactionType.SELL,
+    )
+    logger.info(f'Создана транзакция {transaction}')
+
+    transaction = service.create_transaction(
+        user_id=1,
+        amount=4,
+        transaction_type=TransactionType.BUY,
+    )
+    logger.info(f'Создана транзакция {transaction}')
+
+    report = service.create_transaction_report(
+        user_id=1,
+        start_date=datetime.now(),
+        end_date=datetime.now() + timedelta(days=1),
+    )
+
+    logger.info(f'Создан отчет {report}')
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    logging.info(get_greetings())
+    main()
