e5d77f9ffe15a5e10c4fdb00a9299bfd4176cc06
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 14:58:52 +0300

    Создает test_validate_user_id в unit.test_service

--- src/tests/unit/test_service.py
+++ src/tests/unit/test_service.py
@@ -17,3 +17,24 @@ def service():
     """
     repository = MagicMock()
     return TransactionService(repository)
+
+
+@pytest.mark.parametrize(
+    'user_id', (
+        pytest.param(0, id='id 0'),
+        pytest.param(1, id='id 1'),
+        pytest.param(
+            '1',
+            id='id string',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+        pytest.param(
+            1.1,
+            id='id float',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+    ),
+)
+def test_validate_user_id(user_id, service):
+    """Тест метода _validate_user_id."""
+    service._validate_user_id(user_id)
