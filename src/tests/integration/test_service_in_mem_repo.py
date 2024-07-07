d794015c1521ee92ad82396d0d1280c80b28c87f
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 15:26:25 +0300

    Добавляет фикстуру service в test_service_in_mem_repo

--- src/tests/integration/test_service_in_mem_repo.py
+++ src/tests/integration/test_service_in_mem_repo.py
@@ -4,3 +4,10 @@ import pytest
 
 from app.in_memory_repository import InMemoryRepository
 from app.service import Transaction, TransactionReport, TransactionService, TransactionType  # noqa
+
+
+@pytest.fixture
+def service():
+    """Фикстура для получения экземпляра сервиса."""
+    repository = InMemoryRepository()
+    return TransactionService(repository)
