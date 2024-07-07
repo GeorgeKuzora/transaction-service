8ad6196ac1f747993f58b5d0bb45f5e73077653c
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 14:58:14 +0300

    Создает фикстуру service в unit.test_service

--- src/tests/unit/test_service.py
+++ src/tests/unit/test_service.py
@@ -5,3 +5,15 @@ import pytest
 
 from app.service import TransactionType  # noqa
 from app.service import RepositoryError, Transaction, TransactionReport, TransactionService  # noqa
+
+
+@pytest.fixture(scope='module')
+def service():
+    """
+    Фикстура для получения экземпляра сервиса.
+
+    Возвращает экземпляр сервиса с мок-объектом вместо
+    атрибута repository.
+    """
+    repository = MagicMock()
+    return TransactionService(repository)
