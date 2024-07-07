d72f2d791f45e46615a2807253e56b5688976f91
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 15:27:12 +0300

    Добавляет test_create_transaction в test_service_in_mem_repo

--- src/tests/integration/test_service_in_mem_repo.py
+++ src/tests/integration/test_service_in_mem_repo.py
@@ -11,3 +11,54 @@ def service():
     """Фикстура для получения экземпляра сервиса."""
     repository = InMemoryRepository()
     return TransactionService(repository)
+
+
+@pytest.mark.parametrize(
+    'user_id, amount, transaction_type', (
+        pytest.param(
+            1,
+            1,
+            TransactionType.BUY,
+            id='valid atributes BUY',
+        ),
+        pytest.param(
+            1,
+            1,
+            TransactionType.SELL,
+            id='valid atributes SELL',
+        ),
+        pytest.param(
+            '1',
+            1,
+            TransactionType.BUY,
+            id='invalid user ID',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+        pytest.param(
+            1,
+            -1,
+            TransactionType.BUY,
+            id='invalid amount',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+        pytest.param(
+            1,
+            1,
+            'BUY',
+            id='invalid transaction type',
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+    ),
+)
+def test_create_transaction(user_id, amount, transaction_type, service):
+    """Тест метода create_transaction."""
+    transaction = service.create_transaction(
+        user_id, amount, transaction_type,
+    )
+    assert len(service.repository.transactions) == 1
+    assert transaction.user_id == 1
+    assert transaction.amount == 1
+    assert transaction.transacton_type in {
+        TransactionType.BUY,
+        TransactionType.SELL,
+    }
