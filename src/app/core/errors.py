875e7fbe1f4454703be5faaa6ae5d6c54da3c6f1
GeorgiyKuzora <rafale87@gmail.com>
2024-08-06 09:13:21 +0000

    Обновляет модуль errors.py

--- src/app/core/errors.py
+++ src/app/core/errors.py
@@ -1,4 +1,27 @@
-class RepositoryError(Exception):
+from fastapi import HTTPException, status
+
+
+class ServerError(HTTPException):
+    """Ошибка при проблемах с сервисом."""
+
+    def __init__(
+        self,
+        status_code: int = status.HTTP_503_SERVICE_UNAVAILABLE,
+        detail: str = 'Неизвестная ошибка сервера',
+    ):
+        """
+        Метод инициализации ServerError.
+
+        :param status_code: Код ответа
+        :type status_code: int
+        :param detail: Сообщение
+        :type detail: str
+        """
+        self.status_code = status_code
+        self.detail = detail
+
+
+class RepositoryError(ServerError):
     """
     Исключение возникающее при запросе в хранилище данных.
 
@@ -7,13 +30,46 @@ class RepositoryError(Exception):
     """
 
 
-class ValidationError(Exception):
+class CacheError(ServerError):
+    """Ошибка в кэше."""
+
+
+class ValidationError(HTTPException):
     """Исключение при валидации данных."""
 
+    def __init__(
+        self,
+        status_code: int = status.HTTP_403_FORBIDDEN,
+        detail: str = 'Запрос имеет неверный формат',
+    ):
+        """
+        Метод инициализации UnprocessableError.
 
-class NotFoundError(Exception):
-    """Исключение если данные не найдены."""
+        :param status_code: Код ответа
+        :type status_code: int
+        :param detail: Сообщение
+        :type detail: str
+        """
+        self.status_code = status_code
+        self.detail = detail
 
 
-class CacheError(Exception):
-    """Ошибка в кэше."""
+class NotFoundError(HTTPException):
+    """Исключение возникающее когда запрошенная информация не найдена."""
+
+    def __init__(
+        self,
+        status_code: int = status.HTTP_404_NOT_FOUND,
+        detail: str = 'Запрошенные данные не найдены',
+    ):
+        """
+        Метод инициализации NotFoundError.
+
+        :param status_code: Код ответа
+        :type status_code: int
+        :param detail: Сообщение
+        :type detail: str
+        """
+        self.status_code = status_code
+        self.detail = detail
+
