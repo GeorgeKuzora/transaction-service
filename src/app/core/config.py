9d5c448f7f64910df3162408b1a6887efeaa66d2
GeorgiyKuzora <rafale87@gmail.com>
2024-08-16 15:27:57 +0000

    Удаляет ненужные импорты

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -4,7 +4,6 @@ from functools import lru_cache
 from pathlib import Path
 from typing import Self
 
-import dotenv
 import yaml
 from pydantic import Field, PostgresDsn
 from pydantic_settings import BaseSettings
